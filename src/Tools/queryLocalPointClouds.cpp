5561c578156a4e2c4dee5dd0d97f24743c8558a4
zhenzhen.xiang <zhenzhen.xiang@lingzhitech.com>
2020-02-22 14:44:04 +0800

    replace local search with global search and update the target cloud with accumulated aligned clouds

--- src/Tools/queryLocalPointClouds.cpp
+++ src/Tools/queryLocalPointClouds.cpp
@@ -10,6 +10,7 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/point_types_conversion.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
 #include <pclomp/ndt_omp.h>
 #include <pclomp/gicp_omp.h>
@@ -19,7 +20,9 @@ using namespace Eigen;
 
 typedef pcl::PointXYZ Point;
 typedef pcl::PointCloud<Point> PointCloud;
-typedef PointCloud::Ptr PointCloudPtr;
+
+void readPointCloudFromTxt(string filename, double minDist, double maxDist,
+                           PointCloud::Ptr pointCloud);
 
 int main(int argc, char** argv)
 {
@@ -144,109 +147,59 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  // find the poses in the query range
+  // construct kd-tree for pose searching
+  PointCloud::Ptr poseCloud(new PointCloud());
+  for (auto pose : poses)
+  {
+    Vector3d pos = pose.topRightCorner(3, 1);
+    Point pt;
+    pt.x = pos(0);
+    pt.y = pos(1);
+    pt.z = pos(2);
+    poseCloud->push_back(pt);
+  }
+
   Vector3d queryPosition = queryPose.topRightCorner(3, 1);
+  Point queryPoint;
+  queryPoint.x = queryPosition(0);
+  queryPoint.y = queryPosition(1);
+  queryPoint.z = queryPosition(2);
 
-  // - lower bound
-  int lowerIndexBound = queryIndex;
-  for (int i = queryIndex - 1; i >= 0; i--)
-  {
-    Vector3d tmpPosition = poses[i].topRightCorner(3, 1);
-    if ((tmpPosition - queryPosition).norm() < queryPoseRange)
-      lowerIndexBound = i;
-    else
-      break;
-  }
+  pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+  kdtree.setInputCloud(poseCloud);
 
-  // - upper bound
-  int upperIndexBound = queryIndex;
-  for (int i = queryIndex + 1; i < numPose; i++)
-  {
-    Vector3d tmpPosition = poses[i].topRightCorner(3, 1);
-    if ((tmpPosition - queryPosition).norm() < queryPoseRange)
-      upperIndexBound = i;
-    else
-      break;
-  }
-
-  cout << "Query index: " << queryIndex << endl;
-  cout << "Lower bound index: " << lowerIndexBound << endl;
-  cout << "Upper bound index: " << upperIndexBound << endl;
+  // find the poses in the query range
+  vector<int> pointIdxRadiusSearch;
+  vector<float> dist;
+  int posesInRange = kdtree.radiusSearch(queryPoint, queryPoseRange,
+                                         pointIdxRadiusSearch, dist);
+  cout << "Found poses in range: " << posesInRange << endl;
 
   // compute relative poses to query pose
   vector<Matrix4d> selectedPoses;
-  for (int i = lowerIndexBound; i <= upperIndexBound; i++)
+  for (int i = 0; i < posesInRange; i++)
   {
+    int ind = pointIdxRadiusSearch[i];
     Matrix4d tmpPose =
-        lidarToGps.inverse() * queryPose.inverse() * poses[i] * lidarToGps;
+        lidarToGps.inverse() * queryPose.inverse() * poses[ind] * lidarToGps;
     selectedPoses.push_back(tmpPose);
-  }
-
-  cout << "Total scans in query range: " << selectedPoses.size() << endl;
-
-  // generate color for each pcl
-  vector<int> colorR, colorG, colorB;
-  double stepColor = 255.0 / selectedPoses.size();
-  for (int i = 0; i < selectedPoses.size(); i++)
-  {
-    int value = i * stepColor;
-    int r = value;
-    int g = 255 - value;
-    int b = 0;
-
-    colorR.push_back(r);
-    colorG.push_back(g);
-    colorB.push_back(b);
   }
 
   // read pcl data in the query range
   double minDist = 3.0;
   double maxDist = 100.0;
   vector<PointCloud::Ptr> pointClouds;
-  vector<double> intensities;
-  for (int i = lowerIndexBound; i <= upperIndexBound; i++)
+  for (int i = 0; i < posesInRange; i++)
   {
-    ifstream ifs(pclList[i].c_str());
+    int ind = pointIdxRadiusSearch[i];
 
-    PointCloud::Ptr pointsPCL(new PointCloud());
-    pointsPCL->height = 1;
-    pointsPCL->is_dense = false;
-
-    while (getline(ifs, line))
-    {
-      if (line[0] == 'n')
-        continue;
-
-      stringstream lineStr(line);
-      string tmpStr;
-      vector<double> point;
-      for (int i = 0; i < 4; i++)
-      {
-        getline(lineStr, tmpStr, ',');
-        point.push_back(atof(tmpStr.c_str()));
-      }
-
-      Vector3d pos;
-      pos << point[0], point[1], point[2];
-
-      // ignore points too close or too far
-      if (pos.norm() < minDist || pos.norm() > maxDist)
-        continue;
-
-      intensities.push_back(point[3]);
-
-      Point pPCL;
-      pPCL.x = point[0];
-      pPCL.y = point[1];
-      pPCL.z = point[2];
-      pointsPCL->push_back(pPCL);
-    }
-
-    pointsPCL->width = pointsPCL->size();
-    pointClouds.push_back(pointsPCL);
+    PointCloud::Ptr points(new PointCloud());
+    readPointCloudFromTxt(pclList[ind], minDist, maxDist, points);
+    pointClouds.push_back(points);
   }
 
-  PointCloud::Ptr queryCloud = pointClouds[queryIndex - lowerIndexBound];
+  PointCloud::Ptr queryCloud(new PointCloud());
+  readPointCloudFromTxt(pclList[queryIndex], minDist, maxDist, queryCloud);
 
   // create downsampling filter
   pcl::VoxelGrid<Point> voxelgrid;
@@ -268,6 +221,10 @@ int main(int argc, char** argv)
   vector<PointCloud::Ptr> pointCloudsAccumulated;
   vector<PointCloud::Ptr> pointCloudsAligned;
   vector<Matrix4d> alignedPoses;
+  vector<Matrix4d> alignedAccPoses;
+
+  PointCloud::Ptr queryAccCloud(new PointCloud());
+  *queryAccCloud += *queryCloud;
 
   pcl::visualization::PCLVisualizer visAlign("visAlign");
 
@@ -279,50 +236,62 @@ int main(int argc, char** argv)
                              selectedPoses[i].cast<float>());
     pointCloudsAccumulated.push_back(pclAccumulated);
 
-    // case 2: aline poinclouds with NDT
+    // case 2: align to query poincloud with NDT
     PointCloud::Ptr pclAligned(new PointCloud());
-    if (i != queryIndex - lowerIndexBound)
-    {
-      // downsampling
-      PointCloud::Ptr downsampled(new PointCloud());
-      voxelgrid.setInputCloud(pointClouds[i]);
-      voxelgrid.filter(*downsampled);
-      pointClouds[i] = downsampled;
 
-      // align
-      Matrix4f initTransform = selectedPoses[i].cast<float>();
-      ndt_omp->setInputSource(pointClouds[i]);
-      ndt_omp->setInputTarget(queryCloud);
-      ndt_omp->align(*pclAligned, initTransform);
+    // downsampling
+    PointCloud::Ptr downsampled(new PointCloud());
+    voxelgrid.setInputCloud(pointClouds[i]);
+    voxelgrid.filter(*downsampled);
+    pointClouds[i] = downsampled;
 
-      pointCloudsAligned.push_back(pclAligned);
-      Matrix4f finalTransform = ndt_omp->getFinalTransformation();
-      alignedPoses.push_back(finalTransform.cast<double>());
+    // align
+    Matrix4f initTransform = selectedPoses[i].cast<float>();
+    ndt_omp->setInputSource(pointClouds[i]);
+    ndt_omp->setInputTarget(queryCloud);
+    ndt_omp->align(*pclAligned, initTransform);
 
-      // print status
-      cout << "Cloud #" << i << endl;
-      cout << "score: " << ndt_omp->getFitnessScore() << endl;
-      cout << "initial transform: \n" << initTransform << endl;
-      cout << "final transform: \n" << finalTransform << endl;
-    }
-    else
-    {
-      pclAligned = pointClouds[i];
-      alignedPoses.push_back(selectedPoses[i]);
-      pointCloudsAligned.push_back(pclAligned);
-    }
+    pointCloudsAligned.push_back(pclAligned);
+    Matrix4f finalTransform = ndt_omp->getFinalTransformation();
+    alignedPoses.push_back(finalTransform.cast<double>());
 
-    // display alignment
+    // print status
+    cout << "Aligned Cloud #" << i << endl;
+    cout << "score: " << ndt_omp->getFitnessScore() << endl;
+    cout << "initial transform: \n" << initTransform << endl;
+    cout << "final transform: \n" << finalTransform << endl;
+
+    // case 3: align to accumulated pointclouds with NDT
+    PointCloud::Ptr pclAlignedAcc(new PointCloud());
+
+    // downsampling
+    PointCloud::Ptr downsampled2(new PointCloud());
+    voxelgrid.setInputCloud(queryAccCloud);
+    voxelgrid.filter(*downsampled2);
+    queryAccCloud = downsampled2;
+
+    // align
+    ndt_omp->setInputTarget(queryAccCloud);
+    ndt_omp->align(*pclAlignedAcc, initTransform);
+
+    finalTransform = ndt_omp->getFinalTransformation();
+    alignedAccPoses.push_back(finalTransform.cast<double>());
+    *queryAccCloud += *pclAlignedAcc;
+
+    // print status
+    cout << "Aligned Accumulated Cloud #" << i << endl;
+    cout << "score: " << ndt_omp->getFitnessScore() << endl;
+    cout << "initial transform: \n" << initTransform << endl;
+    cout << "final transform: \n" << finalTransform << endl;
+
+    // display
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
-        accumulated_handler(pclAccumulated, 0.0, 0.0, 255.0);
+        query_handler(queryAccCloud, 255.0, 0.0, 0.0);
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
-        aligned_handler(pclAligned, 0.0, 255.0, 0.0);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
-        query_handler(queryCloud, 255.0, 0.0, 0.0);
+        aligned_handler(pclAlignedAcc, 0.0, 255.0, 0.0);
     visAlign.removeAllPointClouds();
-    visAlign.addPointCloud(pclAccumulated, accumulated_handler, "accumulated");
-    visAlign.addPointCloud(pclAligned, aligned_handler, "aligned");
-    visAlign.addPointCloud(queryCloud, query_handler, "query");
+    visAlign.addPointCloud(queryAccCloud, query_handler, "query");
+    visAlign.addPointCloud(pclAlignedAcc, aligned_handler, "alignedAcc");
     visAlign.spinOnce();
   }
 
@@ -352,6 +321,11 @@ int main(int argc, char** argv)
                                     to_string(queryPoseRange) + ".ply";
   writer.write(mergedAlignedPlyFileName, *mergedAlignedCloud, true);
 
+  string mergedAlignedAccPlyFileName = "data/test/merged_aligned_accumulated_" +
+                                       to_string(queryTimestamp) + '_' +
+                                       to_string(queryPoseRange) + ".ply";
+  writer.write(mergedAlignedAccPlyFileName, *queryAccCloud, true);
+
   string queryPlyFileName =
       "data/test/query_" + to_string(queryTimestamp) + ".ply";
   writer.write(queryPlyFileName, *queryCloud, true);
@@ -366,13 +340,47 @@ int main(int argc, char** argv)
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
       source_handler(mergedAccumulatedCloud, 0.0, 0.0, 255.0);
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
-      aligned_handler(mergedAlignedCloud, 0.0, 255.0, 0.0);
+      aligned_handler(queryAccCloud, 0.0, 255.0, 0.0);
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
       target_handler(queryCloud, 255.0, 0.0, 0.0);
   vis.addPointCloud(mergedAccumulatedCloud, source_handler, "accumulated");
-  vis.addPointCloud(mergedAlignedCloud, aligned_handler, "aligned");
+  vis.addPointCloud(queryAccCloud, aligned_handler, "alignedAcc");
   vis.addPointCloud(queryCloud, target_handler, "query");
   vis.spin();
 
   return 0;
+}
+
+void readPointCloudFromTxt(string filename, double minDist, double maxDist,
+                           PointCloud::Ptr pointCloud)
+{
+  ifstream ifs(filename);
+  string line;
+  while (getline(ifs, line))
+  {
+    if (line[0] == 'n')
+      continue;
+
+    stringstream lineStr(line);
+    string tmpStr;
+    vector<double> point;
+    for (int i = 0; i < 4; i++)
+    {
+      getline(lineStr, tmpStr, ',');
+      point.push_back(atof(tmpStr.c_str()));
+    }
+
+    Vector3d pos;
+    pos << point[0], point[1], point[2];
+
+    // ignore points too close or too far
+    if (pos.norm() < minDist || pos.norm() > maxDist)
+      continue;
+
+    Point pPCL;
+    pPCL.x = point[0];
+    pPCL.y = point[1];
+    pPCL.z = point[2];
+    pointCloud->push_back(pPCL);
+  }
 }
